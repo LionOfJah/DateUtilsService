b83a76265dc28c6631d3d8a0765881c107273081
Himanshu <himanshu04dagur@gmail.com>
2020-05-14 22:26:35 +0530

    Deployment Changes

--- src/main/java/com/icicibank/apimgmt/DateUtilsServiceApplication.java
+++ src/main/java/com/icicibank/apimgmt/DateUtilsServiceApplication.java
@@ -2,12 +2,24 @@ package com.icicibank.apimgmt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DateUtilsServiceApplication {
+public class DateUtilsServiceApplication extends SpringBootServletInitializer{
 
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DateUtilsServiceApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		
+		return builder.sources(DateUtilsServiceApplication.class);
+	}
+	
+	
 
 }
